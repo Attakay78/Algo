904696e0da16f06ba551630381bbca6e3d0447f1
Richard Quaicoe <richard.quaicoe@turntabl.io>
2023-08-21 16:51:31 +0000

    Modified HashMap implementation

--- hash_map.py
+++ hash_map.py
@@ -5,72 +5,91 @@ class Pair:
         self.key = key
         self.value = value
 
-
 class HashMap:
     def __init__(self):
-        self.size = 0
         self.capacity = 2
-        self.map = [None, None]
+        self.size = 0
+        self.storage = [None, None]
     
     def hash(self, key):
-        index = 0
-        for character in key:
-            index += ord(character)
-        
-        return index % self.capacity
+        # Calculate hash of the key using ord method
+        ascii_sum = 0
+        for char in key:
+            ascii_sum += ord(char)
+        return ascii_sum % self.capacity
     
-    def get(self, key):
-        index = self.hash(key)
+    def rehash(self):
+        # Rehash every key in the HashMap
+        self.size = 0
+        self.capacity = self.capacity * 2
 
-        while self.map[index] != None:
-            if self.map[index].key == key:
-                return self.map[index].value
-            
-            index += 1
-            index = index % self.capacity
-        
-        return None
+        old_storage, self.storage = self.storage, [None] * self.capacity
+
+        # Put all pairs in old storage into new storage with bigger capacity
+        for pair in old_storage:
+            if pair:
+                key, value = pair.key, pair.value
+                self.put(key, value)
     
     def put(self, key, value):
-        index = self.hash(key)
+        hash_index = self.hash(key)
 
+        # Loop till we get empty space to insert Pair
         while True:
-            if self.map[index] == None:
+            if self.storage[hash_index] == None:
                 pair = Pair(key, value)
-                self.map[index] = pair
+                self.storage[hash_index] = pair
                 self.size += 1
 
+                # Rehash all keys since size of storage is >= capacity
                 if self.size >= self.capacity // 2:
                     self.rehash()
                     return
             
-            if self.map[index].key == key:
-                self.map[index].value = value
+            # To prevent key duplication, Just change the value
+            if self.storage[hash_index].key == key:
+                self.storage[hash_index].value = value
                 return
             
-            index += 1
-            index = index % self.capacity
+            # Recompute hash_index to prevent index from going out of
+            # bound with the capacity value, resets hash_index to first 
+            # index in storage
+            hash_index += 1
+            hash_index = hash_index % self.capacity
     
-    def rehash(self):
-        self.size = 0
-        self.capacity = self.capacity * 2
-        new_map = []
-
-        for _ in range(self.capacity):
-            new_map.append(None)
+    def get(self, key):
+        hash_index = self.hash(key)
         
-        old_map = self.map
-        self.map = new_map
+        while self.storage[hash_index] != None:
+            if self.storage[hash_index].key == key:
+                return self.storage[hash_index].value
+            
+            # Recompute hash_index to prevent index from going out of
+            # bound with the capacity value, resets hash_index to first 
+            # index in storage
+            hash_index += 1
+            hash_index = hash_index % self.capacity
 
-        for pair in old_map:
-            if pair:
-                self.put(pair.key, pair.value)
+        return None
 
 
 # Testing
 hash_map = HashMap()
-hash_map.put("richie", 23)
-hash_map.put("george", 15)
 
-print(hash_map.get("richie"))
-print(hash_map.get("obed"))
+hash_map.put("Name", "Kofi")
+hash_map.put("Age", 3)
+hash_map.put("Salary", "2500")
+hash_map.put("Position", 'Senior Engineer')
+
+print(hash_map.get("Name"))
+print(hash_map.get("Age"))
+print(hash_map.get("Salary"))
+print(hash_map.get("Position"))
+print("********************************")
+
+# Print HashMap
+for val in hash_map.storage:
+    if val != None:
+        print(val.key, val.value)
+    else:
+        print(val)
